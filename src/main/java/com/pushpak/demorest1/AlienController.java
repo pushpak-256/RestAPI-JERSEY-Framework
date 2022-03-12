65e6008391350cdc9296c7692ac5f5584de03afa
Pushpak Bhagat <pbhagat@montran.com>
2022-03-12 15:53:43 +0530

    Return Alien list insted of one alien object

--- src/main/java/com/pushpak/demorest1/AlienController.java
+++ src/main/java/com/pushpak/demorest1/AlienController.java
@@ -1,5 +1,9 @@
 package com.pushpak.demorest1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -10,7 +14,7 @@ public class AlienController {
 
 	@GET
     @Produces(MediaType.APPLICATION_XML)
-	public Alien getAlien() {
+	public ArrayList<Alien> getAlien() {
 		
 		System.out.println("getAlien called");
 		
@@ -19,6 +23,11 @@ public class AlienController {
 		a1.setName("Pushpak");
 		a1.setPoints(10);
 		
-		return a1;
+		Alien a2 = new Alien();
+
+		a2.setName("Riya");
+		a2.setPoints(12);
+
+		return (new ArrayList<Alien>(Arrays.asList(a1,a2)));
 	}
 }
